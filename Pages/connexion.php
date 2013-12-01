a3a4680dc6cd89c621105f8bd5e9207224833aad
MaxenceBures <maxence.bures@gmail.com>
2013-12-01 20:42:05 +0100

    Mise en place de controles de saisies

--- Pages/connexion.php
+++ Pages/connexion.php
@@ -10,9 +10,9 @@
 <body>
 	<form id="connexion_form" action="<?php $_SERVER['PHP_SELF']; ?>" method="post">
 		<label for="login">Matricule : </label>
-		<input type="text" id="login" name="login"/></br>
+		<input type="text" required="" id="login" name="login"/></br>
 		<label for="password">Mot de passe : </label>
-		<input type="password" id="password" name="password"/></br>
+		<input type="password" required="" id="password" name="password"/></br>
 		<input type="submit" name="go_login" id="go_login" value="Se connecter"/>
 		</form>
 </body>
