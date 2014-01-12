3ac5dccc19380c4e1e7aa4183ab8ccecf63fcfed
MaxenceBures <maxence.bures@gmail.com>
2014-01-12 15:43:37 +0100

    Fonctionnel

--- index.php
+++ index.php
@@ -26,34 +26,23 @@
 ?>
 <html>
 	<head>
-	<meta name="viewport" content="width=device-width, initial-scale=1"> 
-	<style type="text/css">
-/* G - Black Sidebar Theme for jQuery Mobile
- * Available on: https://gist.github.com/4558649
------------------------------------------------------------------------------------------------------------*/
-.ui-bar-g{border:1px solid #666;background:#444;color:#ccc;font-weight:bold;text-shadow:0 -1px 1px #000;background-image:-webkit-gradient(linear,left top,left bottom,from(#555),to(#444));background-image:-webkit-linear-gradient(#555,#444);background-image:-moz-linear-gradient(#555,#444);background-image:-ms-linear-gradient(#555,#444);background-image:-o-linear-gradient(#555,#444);background-image:linear-gradient(#555,#444)}.ui-bar-g,.ui-bar-g input,.ui-bar-g select,.ui-bar-g textarea,.ui-bar-g button{font-family:Helvetica,Arial,sans-serif}.ui-bar-g .ui-link-inherit{color:#fff}.ui-bar-g a.ui-link{color:#7cc4e7;font-weight:bold}.ui-bar-g a.ui-link:visited{color:#2489ce}.ui-bar-g a.ui-link:hover{color:#2489ce}.ui-bar-g a.ui-link:active{color:#2489ce}.ui-body-g,.ui-overlay-g{border:1px solid #444;background:#333;color:#fff;text-shadow:0 1px 1px #111;font-weight:normal}.ui-overlay-g{background-image:none;border-width:0}.ui-body-g,.ui-body-g input,.ui-body-g select,.ui-body-g textarea,.ui-body-g button{font-family:Helvetica,Arial,sans-serif}.ui-body-g .ui-link-inherit{color:#fff}.ui-body-g .ui-link{color:#2489ce;font-weight:bold}.ui-body-g .ui-link:visited{color:#2489ce}.ui-body-g .ui-link:hover{color:#2489ce}.ui-body-g .ui-link:active{color:#2489ce}.ui-btn-up-g{border:1px solid #111;background:#333;font-weight:bold;color:#eee;text-shadow:0 1px 1px #111}.ui-btn-up-g:visited,.ui-btn-up-g a.ui-link-inherit{color:#eee}.ui-btn-hover-g{border:1px solid #000;background:#2e2e2e;font-weight:bold;color:#eee;text-shadow:0 1px 1px #111}.ui-btn-hover-g:visited,.ui-btn-hover-g:hover,.ui-btn-hover-g a.ui-link-inherit{color:#eee}.ui-btn-down-g{border:1px solid #000;background:#222;font-weight:bold;color:#eee;text-shadow:0 1px 1px #111}.ui-btn-down-g:visited,.ui-btn-down-g:hover,.ui-btn-down-g a.ui-link-inherit{color:#eee}.ui-btn-up-g,.ui-btn-hover-g,.ui-btn-down-g{font-family:Helvetica,Arial,sans-serif;text-decoration:none}.ui-btn-up-g .ui-btn-inner,.ui-btn-hover-g .ui-btn-inner,.ui-btn-down-g .ui-btn-inner{border-top:1px solid #fff;border-color:rgba(255,255,255,.15)}
-</style>
-    <link rel="stylesheet" href="Test/jquery.css" />
+	<!--<meta name="viewport" content="width=device-width, initial-scale=1">-->
+
+		<link rel="stylesheet" href="Test/jquery.css" />
     <script src="Test/jquery.js"></script>
     <script src="Test/jquery-mobile.js"></script>
-    <link rel="stylesheet" href="css/style.css">
-    <script src="js/skel.min.js"></script>
-	<script src="js/skel-panels.min.js"></script>
-	<script src="js/init.js"></script>
-	<!--<script src="js/jquery.js"></script>-->
-	<!--	<noscript>
-			<link rel="stylesheet" href="css/skel-noscript.css" />
-			<link rel="stylesheet" href="css/style.css" />
-			<link rel="stylesheet" href="css/style-desktop.css" />
-			<link rel="stylesheet" href="css/style-wide.css" />
-		</noscript>
-<!--<meta name="viewport" content="width=device-width, initial-scale=1"/> 
-	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
-	<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
-	<script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>-->
-
+    	
+		<script src="js/jquery.min.js"></script>
+		<script src="js/jquery182.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-panels.min.js"></script>
+		<script src="js/init.js"></script>
+		<link rel="stylesheet" href="css/style.css" />
 	</head>
-
+<!--<script src="js/skel.min.js"></script>
+	<script src="js/skel-panels.min.js"></script>
+	<script src="js/init.js"></script>-->
+	
 	<body class="left-sidebar">
 		<div id="content">
 			<div id="content-inner">
