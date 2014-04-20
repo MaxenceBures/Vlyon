67b89f9a9813754bb2d2e4ec1b12fc64487778be
MaxenceBures <maxence.bures@gmail.com>
2014-04-20 14:53:19 +0200

    Fonctionnel

--- html/header.php
+++ html/header.php
@@ -5,7 +5,7 @@
 	<link rel="stylesheet" href="css/jquery.css" />
     <script src="js/jquery.js"></script>
     <script src="js/jquery-mobile.js"></script>
-  <!--  <script src="js/jquery.min.js"></script>-->
+   <script src="js/jquery.min.js"></script>
 	<script src="js/skel.min.js"></script>
 	<script src="js/skel-panels.min.js"></script>
 	<script src="js/init.js"></script>
