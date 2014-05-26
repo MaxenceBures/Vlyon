5cd7504a1a6a697d7a28e4a2d1884697aa4361f5
MaxenceBures <maxence.bures@gmail.com>
2014-05-26 20:54:09 +0200

    Indentation

--- FO/VUES/Intervention/fo_ModifIntervention.php
+++ FO/VUES/Intervention/fo_ModifIntervention.php
@@ -27,73 +27,64 @@ if(isset($_SESSION['id']))
 			<td>
 				<input type="text" id="velo" name="velo" disabled="" value="<?php echo($enreg["BI_VELO"]); ?>"/>
 			</td>
-				</tr>
-				</br>
-				<tr>
-					<td>
-						<label for="dfin">Date Fin: </label>
-					</td>
-					<td>
-						<input type="date" id="dfin" value="<?php echo($enreg['BI_DATFIN']) ?>" name="dfin"/>
-					</td>
-				</tr>
-				</br>
-				<tr>
-					<td>
-						<label for="compterendu">Compte rendu : </label>
-					</td>
-					<td>
-						<textarea rows="4" cols="50"id="compterendu" name="compterendu"><?php echo($enreg['BI_CPTERENDU']) ?></textarea>
-					</td>
-				</tr>
-
-				</br>
-				<tr>
-					<td>
-						<label for="reparable">Reparable </label>
-					</td>
-					<td>
-						<input type="checkbox" id="reparable"  value="<?php echo($enreg['BI_REPARABLE']) ?>"name="reparable"/>
-					</td>
-				</tr>
-					</br>
-				<tr>
-					<td>
-						<label for="demande">Demande </label>
-					</td>
-					<td>
-						<input type="checkbox" id="demande"  value="<?php echo($enreg['BI_DEMANDE']) ?>" name="demande"/>
-					</td>
-				</tr>
-					</br>
-				<tr>
-					<td>
-						<label for="surplace">Sur Place </label>
-					</td>
-					<td>
-						<input type="checkbox" value="<?php echo($enreg['BI_SURPLACE'])?>" id="surplace" name="surplace"/>
-					</td>
-				</tr>
-					</br>
-				<tr>
-					<td>
-						<label for="duree">Dur&eacute;e </label>
-					</td>
-					<td>
-						<input type="text" required="" value="<?php echo($enreg['BI_DUREE'])?>" id="duree" name="duree"/>
-					</td>
-				</tr>
-			</table>
-			</br>
-						<input type="submit" name="go_modifint" id="go_modifint" value="Modifier"/>
+			</tr>
+			<tr>
+				<td>
+					<label for="dfin">Date Fin: </label>
+				</td>
+				<td>
+					<input type="date" id="dfin" value="<?php echo($enreg['BI_DATFIN']) ?>" name="dfin"/>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<label for="compterendu">Compte rendu : </label>
+				</td>
+				<td>
+					<textarea rows="4" cols="50"id="compterendu" name="compterendu"><?php echo($enreg['BI_CPTERENDU']) ?></textarea>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<label for="reparable">Reparable </label>
+				</td>
+				<td>
+					<input type="checkbox" id="reparable"  value="<?php echo($enreg['BI_REPARABLE']) ?>"name="reparable"/>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<label for="demande">Demande </label>
+				</td>
+				<td>
+					<input type="checkbox" id="demande"  value="<?php echo($enreg['BI_DEMANDE']) ?>" name="demande"/>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<label for="surplace">Sur Place </label>
+				</td>
+				<td>
+					<input type="checkbox" value="<?php echo($enreg['BI_SURPLACE'])?>" id="surplace" name="surplace"/>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<label for="duree">Dur&eacute;e </label>
+				</td>
+				<td>
+					<input type="text" required="" value="<?php echo($enreg['BI_DUREE'])?>" id="duree" name="duree"/>
+				</td>
+			</tr>
+		</table>
+		<input type="submit" name="go_modifint" id="go_modifint" value="Modifier"/>
 		</form>
-	<!--</body>-->
 	</div>
 
 <?php
 }
 else
 {
-header('Location:/Vlyon/Pages/connexion.php');
+	header('Location:/Vlyon/Pages/connexion.php');
 }
 ?>
--- FO/VUES/Intervention/pagination_Intervention.php
+++ FO/VUES/Intervention/pagination_Intervention.php
@@ -1,59 +1,47 @@
 <?php
-
 include('../../../include/functions.inc.php');
-
 
 if($_GET)
 {
-$page=$_GET['page'];
+	$page=$_GET['page'];
 }
-
-
-
-
 ?>
-<!--<div data-role="page">
-<a href="?page=accueil"><img src="css/Home.png" border="0" align="center" width=42 height=42></img></a></br>-->
 
-<b>Intervention </b>
-			<table class="style1">
-				<tr>
-					<th width="5%">Num</th>
-					<th width="5%">Velo</th>
-					<th width="8%">Debut</th>
-					<th width="8%">Fin</th>
-					<th width="5%">Durée</th>
-					<th width="15%">Compte Rendu</th>
-					<th width="5%">Rep</th>
-					<th width="5%">Dem</th>
-					<th width="5%">Tech</th>
-					<th width="5%">Sur Place</th>
-
-
-				</tr>
-					<form  action="<?php $_SERVER['PHP_SELF']; ?>" name="ListeInt" method="POST">
-		<?php
-$rsd = pagination_InterventionListe($page);
-		while($listes = mysql_fetch_array($rsd))
+	<b>Intervention </b>
+		<table class="style1">
+			<tr>
+				<th width="5%">Num</th>
+				<th width="5%">Velo</th>
+				<th width="8%">Debut</th>
+				<th width="8%">Fin</th>
+				<th width="5%">Durée</th>
+				<th width="15%">Compte Rendu</th>
+				<th width="5%">Rep</th>
+				<th width="5%">Dem</th>
+				<th width="5%">Tech</th>
+				<th width="5%">Sur Place</th>
+			</tr>
+				<form  action="<?php $_SERVER['PHP_SELF']; ?>" name="ListeInt" method="POST">
+<?php
+				$rsd = pagination_InterventionListe($page);
+				while($listes = mysql_fetch_array($rsd))
 				{
-		?>
-				<tr>
-					<td><?php echo $listes["BI_NUM"]; ?></td>
-					<td><?php echo $listes["BI_VELO"]; ?></td>
-					<td><?php echo datefr($listes["BI_DATDEBUT"]); ?></td>
-					<td><?php echo datefr($listes["BI_DATFIN"]) ; ?></td>
-					<td><?php echo $listes["BI_DUREE"] ;?></td>
-					<td><?php echo substr($listes["BI_CPTERENDU"], 0, 15);if(strlen($listes["BI_CPTERENDU"]) > 15){echo " ...";}; ?></td>
-					<td><?php if($listes["BI_REPARABLE"] == 0){echo "non";}else{echo"oui";} ; ?></td>
-					<td><?php if($listes["BI_DEMANDE"] == 0){echo "non";}else{echo"oui";} ;?></td>
-					<td><?php echo $listes["TEC_NOM"] ;?></td>
-					<td><?php if($listes["BI_SURPLACE"] == 0){echo "non";}else{echo"oui";} ;?></td>
-					<td><a href="?page=ModifInter&variable=<?php print($listes["BI_NUM"]); ?>"><input type="image" src="./images/modif.png" value="Voir" width="30" height="30"></a></td><!--<input type="button" value="Voir"  />-->
-
-				</tr>
-		<?php
+?>
+			<tr>
+				<td><?php echo $listes["BI_NUM"]; ?></td>
+				<td><?php echo $listes["BI_VELO"]; ?></td>
+				<td><?php echo datefr($listes["BI_DATDEBUT"]); ?></td>
+				<td><?php echo datefr($listes["BI_DATFIN"]) ; ?></td>
+				<td><?php echo $listes["BI_DUREE"] ;?></td>
+				<td><?php echo substr($listes["BI_CPTERENDU"], 0, 15);if(strlen($listes["BI_CPTERENDU"]) > 15){echo " ...";}; ?></td>
+				<td><?php if($listes["BI_REPARABLE"] == 0){echo "non";}else{echo"oui";} ; ?></td>
+				<td><?php if($listes["BI_DEMANDE"] == 0){echo "non";}else{echo"oui";} ;?></td>
+				<td><?php echo $listes["TEC_NOM"] ;?></td>
+				<td><?php if($listes["BI_SURPLACE"] == 0){echo "non";}else{echo"oui";} ;?></td>
+				<td><a href="?page=ModifInter&variable=<?php print($listes["BI_NUM"]); ?>"><input type="image" src="./images/modif.png" value="Voir" width="30" height="30"></a></td><!--<input type="button" value="Voir"  />-->
+			</tr>
+<?php
 				}
-		?>
-					</form>
-			</table>
-
+?>
+				</form>
+		</table>
